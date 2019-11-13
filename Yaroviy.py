08876cf838d69d86d564a4e2095d7257aaf27080
Яровий Данило <57644163+danyayaroviy@users.noreply.github.com>
2019-11-13 11:19:56 +0200

    Add files via upload
    kr 2

---
+++ Yaroviy.py
@@ -0,0 +1,27 @@
+def kol1(text)
+    text2=[]
+    for sl in text:
+       kol=0
+        for ns in text:
+            if slovo=ns:
+                kol +=1
+                text2.append([slovo])
+                text2=set(text2)
+                return text2
+
+def ml(text):
+    for pnom in range (len(text))
+        text[pnom]=text[pnom].lower()
+        return(text)
+
+
+znaki=[" ", ",", ".", ":", ";", "!", "?" ,"-"]
+text=input("")
+text=text.split(" ")
+
+for i in range (len(text)):
+    text[i] = text[i].lower()
+
+text2=[]
+
+
